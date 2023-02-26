3eb73405655b2bd6bff5bc657652ffd9149e1ab1
cpkopp <cameronpkopp@gmail.com>
2023-02-26 03:58:04 -1000

    2d data gen works great

--- sample_generator_2D.py
+++ sample_generator_2D.py
@@ -11,6 +11,7 @@ from math import *
 from pylab import *
 import matplotlib.pyplot as plt
 from matplotlib import colors
+from mpl_toolkits import mplot3d
 
 # =============================================================================
 # SAMPLE CONSTRUCTOR CLASS 
@@ -35,7 +36,7 @@ class SampleConstructor2D:
                         our gaussian) *NOTE* to simplify the nature of the 
                         Gaussian probabilty function, the same spring constant
                         applies to both reaction coordinates,
-        sampleNumber (number of samples taken from each simulation)
+        sampleNumber (number of samples taken from each simulation, i)
     """
     
     def __init__(self, xmin, xmax, ymin, ymax, simNumberX, simNumberY, binSizeX,
@@ -55,7 +56,7 @@ class SampleConstructor2D:
         self.bsX = binSizeX
         self.bsY = binSizeY
         self.K = springConstant
-        self.N = sampleNumber
+        self.Ni = sampleNumber
         
         #constuct coordinate system, don't need to worry about combining
         #reaction coordinates until setting up binned histogram, will always
@@ -89,6 +90,173 @@ class SampleConstructor2D:
         histShape = (self.biX.size, self.biY.size)
         self.hist = zeros(histShape, dtype=int)
         
+        return
+        
+        
+        
+        
+    def biasPotential(self, xt, yt, mx, my):
+        """
+        Parameters
+        ----------
+        xt : float
+            value of x reaction cordinate randomly generated from 
+            gaussian dist
+        yt : float
+            value of y reaction cordinate randomly generated from 
+            gaussian dist
+        mx: float
+            value of the center of the potential along x reaction 
+            coordinate
+        mx: float
+            value of the center of the potential along y reaction 
+            coordinate
+
+        Returns
+        -------
+        returns the probability distribution value associated with the
+        harmonic biasing potential for the given test points
+
+        """
+        #constants: c1= standard deviation in terms of spring constant k
+        #nc = normalization constant in two dimensions of equal 
+        #   standard deviations (factor of 2 carried in with c1)
+        c1 = self.K/2
+        nc = c1/math.pi
+        
+        #contribution to wheighted prob/ dist/ function from biasing
+        baisDist = nc *exp(-c1*(((xt-mx)**2)+(yt-my)**2))
+        
+        return baisDist
+    
+    def smoothTestFunction(self, xt, yt):
+        """
+        Will serve as our known unbiased probability distribution
+        
+        Need to make sure this function is always less than to use 
+            accept-reject method
+
+        Parameters
+        ----------
+        xt : float
+            value of x reaction cordinate randomly generated from 
+            gaussian dist
+        yt : float
+            value of y reaction cordinate randomly generated from 
+            gaussian dist
+
+        Returns
+        -------
+        The value of a messy and arbitrary function selected for its smoothness
+        and that it is bounded by (0,1)
+
+        """
+        #NOTE THAT NORMALIZATION CONSTAN IS GENERATED NUMERICALLY
+        #   BY INTEGRATING OF MIN AND MAX OF REACTION COORDS
+        nc = 1/2.51388
+        
+        unbiasedDist = ((1/(1+(1-xt)**2)) * (((math.sin(3*xt)**2)+2)/3) * 
+                        (1/(1+(2-yt)**2)) * (((math.sin(3*yt)**2)+2)/3))
+        
+        return unbiasedDist
+    
+    
+    def dateGen2D(self):
+        """
+        Relies on a accept-reject algorithm, applied a few techniques to try 
+        to speed it up but for large systems it will take some time.
+
+        Returns
+        -------
+        A histogram corresponding to the binning of the samples generated 
+        through this mock biased MD simulation along two reaction coordinates,
+        it will be a [[biX.size X biY.size] 2d array of integers, and will be
+        the resulting sumation over all of the simulations
+
+        """
+        
+        #func for random gaussian points wants K -> sigma
+        stdev = 1/sqrt(self.K)
+        #covariance matrix
+        cvmatrix =[[stdev**2,0],[0,stdev**2]];
+        
+        #each iteration through double for loop coresponds to the simulation
+        #with the biasing potential centered at (mX,mY)
+        
+        for mX in self.muX:
+            
+            for mY in self.muY:
+                
+                print('(muX,muY) is ({},{})'.format(mX,mY))
+                
+                #reset accepted sample counter
+                i = 0
+                
+                #loop until i reaches self.Ni = sample num per sim
+                while(i < self.Ni):
+                    
+                    #generate test points for sample
+                    xT, yT = r.multivariate_normal((mX, mY), cvmatrix)
+                    
+                    tval = (self.biasPotential(xT, yT, mX, mY) * 
+                            self.smoothTestFunction(xT, yT))
+                    #We know that tval will always be bounded by
+                    #the value of the biasPotential dist alone,
+                    #so the accept-reject method supports what follows
+                    uniformT = r.uniform(0, self.biasPotential(xT, yT, mX, mY))
+                    
+                    #accept-reject condition
+                    if (tval > uniformT):
+                        #also check to make sure points land in histogram
+                        if (xT < self.xmx) and (xT > self.xmn):
+                            if (yT < self.ymx) and (yT > self.ymn):
+                                #now we accept the point as valid sample
+                                i = i + 1
+                                #need to match xT, yT floats to a bin index
+                                histBinFloatX = (xT/self.bsX)
+                                histBinX = floor(histBinFloatX)
+                                indexX = int(histBinX)
+                                
+                                histBinFloatY = (yT/self.bsY)
+                                histBinY = floor(histBinFloatY)
+                                indexY = int(histBinY)
+                                
+                                #update histogram
+                                self.hist[indexX, indexY] = (
+                                    self.hist[indexX, indexY] + 1)
+                                
+                                #print a message to track progress
+                                if (i == floor(self.Ni/2)):
+                                    print(
+                                        'half done on ({},{})'.format(mX,mY))
+                    
+        plt.imshow(self.hist, cmap='magma')
+        plt.title( "2-D Heat Map" )
+        plt.xlabel('x-axis')
+        plt.ylabel('y-axis')
+        plt.colorbar()
+        plt.show
+        
+        return self.hist
+        
+        
+        
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
 #%%
 # =============================================================================
@@ -100,19 +268,15 @@ x0 = 0
 xN = 3
 y0 = 0
 yN = 3
-snX = 5
-snY = 5
-bsX =.5
-bsY =.5
+snX = 30
+snY = 30
+bsX =1/100
+bsY =1/100
 k = 1
-samps =9
+samps =900
 
 t1 =   SampleConstructor2D(x0, xN, y0, yN,snX,snY,bsX,bsY,k,samps) 
-print(t1.bcY) 
-
-print(t1.muX)    
-print(t1.hist)
-        
+t1.dateGen2D()
         
         
         
